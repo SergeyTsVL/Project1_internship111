4c10b3e39b9ee334d1481142c6111a793824dd00
SergeyTsVL <tsarskiytsarskiy@mail.ru>
2024-10-21 15:19:04 +0700

    333

--- data_download.py
+++ data_download.py
@@ -1,12 +1,24 @@
+# -*- coding: utf-8 -*-
 import yfinance as yf
 
-
 def fetch_stock_data(ticker, period='1mo'):
+    """
+    Метод определяет за какой период сделать выборку из "DataFrame" даных об акции, в "data" передает все данные по
+    акциям. Допустимые периоды: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max с начала года, максимум
+    Допустимые интервалы: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
+    """
     stock = yf.Ticker(ticker)
     data = stock.history(period=period)
     return data
 
 
 def add_moving_average(data, window_size=5):
+    """
+    Метод определяет цену закрытия по акциям, берет в data в колонке "Close".
+    "rolling(window=window_size).mean()" обозначает расчёт скользящего среднего с указанным размером окна (window_size).
+    :param data:
+    :param window_size:
+    :return:
+    """
     data['Moving_Average'] = data['Close'].rolling(window=window_size).mean()
     return data
--- data_plotting.py
+++ data_plotting.py
@@ -1,48 +1,44 @@
-from colorama import Fore
 import matplotlib.pyplot as plt
 import pandas as pd
+
 import logging
-# import Path
+from colorama import Fore
 
 def create_and_save_plot(data, ticker, period, filename=None):
-
     plt.figure(figsize=(10, 6))
-
     if 'Date' not in data:
         if pd.api.types.is_datetime64_any_dtype(data.index):
             dates = data.index.to_numpy()
             plt.plot(dates, data['Close'].values, label='Close Price')
             plt.plot(dates, data['Moving_Average'].values, label='Moving Average')
-
-
         else:
             print("Информация о дате отсутствует или не имеет распознаваемого формата.")
             return
     else:
         if not pd.api.types.is_datetime64_any_dtype(data['Date']):
             data['Date'] = pd.to_datetime(data['Date'])
+        else:
+            return None
         plt.plot(data['Date'], data['Close'], label='Close Price')
         plt.plot(data['Date'], data['Moving_Average'], label='Moving Average')
-
     plt.title(f"{ticker} Цена акций с течением времени")
     plt.xlabel("Дата")
     plt.ylabel("Цена")
     plt.legend()
-
     if filename is None:
         filename = f"{ticker}_{period}_stock_price_chart.png"
-
-    plt.savefig(filename)
+    else:
+        return None
+    plt.savefig(f"images/{filename}")
     print(f"График сохранен как {filename}")
 
-def calculate_and_display_average_price(data):
 
+def calculate_and_display_average_price(data):
     try:
         a = data['Close'].values()
         period_prices = sum(a) / len(a)
-        print(f'Cреднее значение колонки \'Close\' за период составляет: {period_prices}')
+        print(f'Cреднее значение колонки "Close" за период составляет: {period_prices}')
         logging.info(f"Среднее значение за период {period_prices}")
-
     except BaseException as err:
         logging.error(f"Произошла ошибка {err} определения среднего значения за период!", exc_info=True)
         return 0
@@ -51,7 +47,9 @@ def calculate_and_display_average_price(data):
 def notify_if_strong_fluctuations(data, threshold):
     a = data['Close'].values
     if float(max(a)) - float(min(a)) > threshold:
-        print(Fore.RED + f"Превышено максимальное пороговое значения между максимальной и минимальной ценой закрытия!" + Fore.WHITE)
+        print(Fore.RED + f"Превышено максимальное пороговое значения между максимальной и минимальной ценой закрытия!"
+              + Fore.WHITE)
+
 
 def export_data_to_csv(data, filename):
 
